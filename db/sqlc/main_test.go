f12194787b6c655805d7cc044ff94a5eb5de7598
hariprathap-hp <hariprathap89@gmail.com>
2022-08-17 12:43:56 +0530

    gitlab config

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	dbDriver = "postgres"
-	dbSource = "postgresql://postgres:postgres@localhost:5432/simple_bank?sslmode=disable"
+	dbSource = "postgresql://postgres:postgres@localhost:8000/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
