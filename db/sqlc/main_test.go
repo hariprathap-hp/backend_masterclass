7d5d49c87dc8631adda7ffefdc0b4dd5b4337555
hariprathap-hp <hariprathap89@gmail.com>
2022-08-17 20:29:46 +0530

    viper added for env variables

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/hariprathap-hp/backend_masterclass/util"
 	_ "github.com/lib/pq"
 )
 
@@ -18,8 +19,13 @@ var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatalln(err)
 	}
